962ae62ec0a7adedafb17a785b3ff4a2c907ef83
iaconom821 <iaconom821@gmail.com>
2021-06-30 14:27:10 -0400

    random position of dog after click

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ function App() {
   const dogImg = <img className="dogPic" src={doge} alt="doge" onClick={(e) => handleClick(e)} /> 
 
   function handleClick (e) {
-    const posx = (Math.random() * (document.body.offsetWidth - 38)).toFixed();
-    const posy = (Math.random() * (document.body.offsetHeight - 38)).toFixed();
+    const posx = (Math.random() * (document.getElementById("dogContainer").offsetWidth - 38)).toFixed();
+    const posy = (Math.random() * (document.getElementById("dogContainer").offsetHeight - 38)).toFixed();
     debugger;
-    e.target.style.position = "absolute"
+    e.target.style.position = "relative"
     e.target.style.left = `${posx}px`
-    e.target.style.bottom = `${posy}px`
+    e.target.style.top = `${posy}px`
     setPlayerScore((oldScore => oldScore += 1))
   }
 
   return ( 
     <>
     <h1>{playerScore}</h1>
+    <div className="dogContainer" id="dogContainer">
     {dogImg}
+    </div>
     </>
   )
 }
