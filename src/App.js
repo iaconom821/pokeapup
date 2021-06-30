c632abe313ccff2f38b3d3477436b330c880a949
iaconom821 <iaconom821@gmail.com>
2021-06-30 12:58:02 -0400

    initial commit and click functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import doge from './doge.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [playerScore, setPlayerScore] = useState(0)
+
+  const dogImg = <img className="dogPic" src={doge} alt="doge" onClick={(e) => handleClick(e)} /> 
+
+  function handleClick (e) {
+    const posx = (Math.random() * (document.body.offsetWidth - 38)).toFixed();
+    const posy = (Math.random() * (document.body.offsetHeight - 38)).toFixed();
+    debugger;
+    e.target.style.position = "absolute"
+    e.target.style.left = `${posx}px`
+    e.target.style.bottom = `${posy}px`
+    setPlayerScore((oldScore => oldScore += 1))
+  }
+
+  return ( 
+    <>
+    <h1>{playerScore}</h1>
+    {dogImg}
+    </>
+  )
 }
 
 export default App;
