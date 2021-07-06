4a2fc5a327659223b982d7f4aacf2cdfc3c4702a
iaconom821 <iaconom821@gmail.com>
2021-07-06 12:54:29 -0400

    update app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
-import { useState } from 'react'
-import './App.css';
-import doge from './doge.png'
+import { useState } from "react";
+import "./App.css";
+import doge from "./doge.png";
 
 function App() {
-  const [playerScore, setPlayerScore] = useState(0)
+  const [playerScore, setPlayerScore] = useState(0);
 
-  const dogImg = <img className="dogPic" src={doge} alt="doge" onClick={(e) => handleClick(e)} /> 
+  const dogImg = (
+    <img
+      className="dogPic"
+      src={doge}
+      alt="doge"
+      onClick={(e) => handleClick(e)}
+    />
+  );
 
-
-  function handleClick (e) {
-    const posx = (Math.random() * (document.getElementById("dogContainer").offsetWidth - 38)).toFixed();
-    const posy = (Math.random() * (document.getElementById("dogContainer").offsetHeight - 38)).toFixed();
-    debugger;
-    e.target.style.position = "relative"
-    e.target.style.left = `${posx}px`
-    e.target.style.top = `${posy}px`
-    setPlayerScore((oldScore => oldScore += 1))
+  function handleClick(e) {
+    const posx = (
+      Math.random() *
+      (document.getElementById("dogContainer").offsetWidth - 38)
+    ).toFixed();
+    const posy = (
+      Math.random() *
+      (document.getElementById("dogContainer").offsetHeight - 38)
+    ).toFixed();
+    e.target.style.position = "relative";
+    e.target.style.left = `${posx}px`;
+    e.target.style.top = `${posy}px`;
+    setPlayerScore((oldScore) => (oldScore += 1));
   }
 
-  
-
-  return ( 
+  return (
     <>
-    <h2>Poke-a-Pup</h2>
-    <h3>Pup Poked {playerScore} times!</h3>
-    <div className="dogContainer" id="dogContainer">
       <div id="clouds">
-        {dogImg}
+        <h2 className="titleText">Poke-a-Pup</h2>
+        <h3 className="titleText">Pup Poked {playerScore} times!</h3>
+        <div className="dogContainer" id="dogContainer">
+          {dogImg}
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
 export default App;
+
